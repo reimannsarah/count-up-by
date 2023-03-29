675b6811cd02989d449c06092f4d78eaaff51799
Sarah Reimann <reimannsarah45@gmail.com>
2023-03-29 13:55:07 -0700

    add ui logic to display number array
    co-authored-by: Emma Gerigscott <elgerig@gmail.com>

--- js/script.js
+++ js/script.js
@@ -16,3 +16,25 @@ function count(number1, number2) {
 }
 
 //UI Logic
+
+// get our input values
+// print output to div#output
+function userInput(e) {
+    e.preventDefault();
+    const input1 = parseInt(document.querySelector("#numberOne").value);
+    const input2 = parseInt(document.querySelector("#numberTwo").value);
+    
+    const results = count(input1, input2);
+    displayAnswer(results);
+}
+
+function displayAnswer(output) {
+    const div = document.querySelector("#output");
+    const p = document.createElement("p");
+    p.innerText = output.join(", ");
+    div.append(p);
+}
+
+window.addEventListener("load", function() {
+    document.querySelector("form").addEventListener("submit", userInput)
+})
